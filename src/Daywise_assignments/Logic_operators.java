191662496e58609d53513220e25fb7ce5fe4e79d
Gitsrc19 <shabanabegum.mca@gmail.com>
2026-09-24 22:01:02 +0530

    operat

---
+++ src/Daywise_assignments/Logic_operators.java
@@ -0,0 +1,17 @@
+package Daywise_assignments;
+
+public class Logic_operators {
+    public static void main(String[] args){
+        int age =15;
+        boolean is_age_18 =age>=18;
+        boolean is_age1_18 = age<=18;
+        boolean compare_age =is_age_18 && is_age_18;
+
+        System.out.println(compare_age);
+
+        int a=10;
+        int b=3;
+        System.out.println(a>=b? "true":"false");
+
+    }
+}
---
+++ src/Daywise_assignments/assignmet_operators.java
@@ -0,0 +1,23 @@
+package Daywise_assignments;
+
+public class assignmet_operators {
+
+    public static void main(String[] args){
+        int a=50;
+        a +=3;
+        System.out.println(a);
+
+        a -=3;
+        System.out.println(a);
+
+        a *=3;
+        System.out.println(a);
+
+        a /=3;
+        System.out.println(a);
+        a %=3;
+        System.out.println(a);
+
+
+    }
+}
